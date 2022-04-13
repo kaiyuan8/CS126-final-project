83630f8ae752bc9c7869cb9f431492ebffd9174f
kaiyuan8 <kaiyuan8@illinois.edu>
2022-04-12 21:42:36 -0500

    Finished Person class functionality for movement

--- include/person.h
+++ include/person.h
@@ -12,18 +12,31 @@ namespace covidsim {
  */
 class Person {
 public:
-    Person();
+    Person(const std::string &status, float speed, const std::vector<vec2> &routes,
+           const std::vector<bool> &health);
 
-    Person(const std::string &status, const std::vector<vec2> &routes, int routeIdx, const std::vector<bool> &health);
+    const vec2 &getPosition() const;
+
+    const vec2 &getVelocity() const;
+
+    const std::string &getStatus() const;
 
 private:
+  glm::vec2 position_;
+  glm::vec2 velocity_;
+
   std::string status_;
-  std::vector<vec2> routes_;
   int route_idx_;
+  float speed_;
+  std::vector<vec2> routes_;
+
   // only three checkers: has_mask_, has_vaccine_, has_immune_
   std::vector<bool> health_;
 
   const float kDefaultSize = 10;
+  const float kDefaultMargin = 5;
+
+  void UpdateRoute();
 };
 
 }  // namespace idealgas
--- src/person.cc
+++ src/person.cc
@@ -2,8 +2,35 @@
 
 namespace covidsim {
 
-Person::Person(const std::string &status, const std::vector<vec2> &routes, int routeIdx,
-           const std::vector<bool> &health) : status_(status), routes_(routes),
-           route_idx_(routeIdx), health_(health) {}
+Person::Person(const std::string &status, float speed,
+               const std::vector<vec2> &routes, const std::vector<bool> &health) :
+               status_(status), speed_(speed),
+               routes_(routes), health_(health) {
+  assert(routes.size() >= 2);
+  position_ = routes_[0];
+  velocity_ = glm::normalize(routes_[1] - routes_[0]) * speed_;
+  route_idx_ = 1;
+}
+
+const vec2 &Person::getPosition() const {
+  return position_;
+}
+
+const vec2 &Person::getVelocity() const {
+  return velocity_;
+}
+
+const std::string &Person::getStatus() const {
+  return status_;
+}
+
+void Person::UpdateRoute() {
+  if (glm::distance(position_, routes_[route_idx_]) <= kDefaultMargin) {
+    position_ = routes_[route_idx_];
+    int new_idx = (route_idx_ + 1) % routes_.size();
+    velocity_ = glm::normalize(routes_[new_idx] - routes_[route_idx_]) * speed_;
+    route_idx_ = new_idx;
+  }
+}
 
 }  // namespace idealgas
