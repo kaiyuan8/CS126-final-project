5b93f160966c2b5db0ef96e43b03bee846a56283
kaiyuan8 <kaiyuan8@illinois.edu>
2022-04-19 21:25:03 -0500

    Added add person functionality

--- include/covid_simulation_app.h
+++ include/covid_simulation_app.h
@@ -16,19 +16,14 @@ class CovidSimApp : public ci::app::App {
 
   void draw() override;
   void update() override;
+  void keyDown(ci::app::KeyEvent event) override;
 
   const int kWindowSize = 875;
   const int kMargin = 100;
+  const int kMaxRoute = 10;
 
  private:
   AreaContainer container_;
-
-  std::vector<vec2> defaultRoutes = {vec2(50, 50), vec2(50, 500),
-                                     vec2(400, 50), vec2(400, 500),
-                                     vec2(800, 50), vec2(800, 500)};
-  std::vector<int> defaultShuffle = {0, 1, 2, 3, 4, 5};
-
-  const int kDefaultPopulationSize = 5;
 };
 
 }  // namespace idealgas
--- src/covid_simulation_app.cc
+++ src/covid_simulation_app.cc
@@ -4,19 +4,6 @@ namespace covidsim {
 
 CovidSimApp::CovidSimApp() {
   ci::app::setWindowSize(kWindowSize, kWindowSize);
-  std::vector<vec2> routes_1{ vec2()};
-  for (int i = 0; i < kDefaultPopulationSize; i++) {
-    std::random_shuffle(defaultShuffle.begin(), defaultShuffle.end());
-    std::vector<vec2> routes;
-    for (auto each : defaultShuffle) {
-      routes.push_back(defaultRoutes[each]);
-    }
-    ci::Color color(float(0), float((i + 1) * 30), float(0));
-    Person* p = new Person(color, float(i + 1), routes,
-                          std::vector<bool>({false, false, false}));
-    container_.AddPerson(p);
-  }
-
 }
 
 void CovidSimApp::draw() {
@@ -28,6 +15,34 @@ void CovidSimApp::draw() {
 
 void CovidSimApp::update() {
   container_.AdvanceOneFrame();
+}
+
+void CovidSimApp::keyDown(cinder::app::KeyEvent event ) {
+  if(event.getChar() == 'a' ) {
+    ci::Color color("green");
+    std::vector<vec2> routes;
+    for (int i = 0; i < kMaxRoute; i++) {
+      float x_position = static_cast<float>(rand() %
+              static_cast<int>(kWindowSize - kMargin) + kMargin / 2);
+      float y_position = static_cast<float>(rand() %
+              static_cast<int>(kWindowSize - kMargin) + kMargin / 2);
+      routes.push_back(vec2(x_position, y_position));
+    }
+    Person* p = new Person(color, 1, routes,
+                           std::vector<bool>({false, false, false}));
+    container_.AddPerson(p);
+  }
+//  else if( event.getCode() == KeyEvent::KEY_SPACE ) {
+//    // Clear the list of points when the user presses the space bar.
+//    mPoints.clear();
+//  }
+//  else if( event.getCode() == KeyEvent::KEY_ESCAPE ) {
+//    // Exit full screen, or quit the application, when the user presses the ESC key.
+//    if( isFullScreen() )
+//      setFullScreen( false );
+//    else
+//      quit();
+//  }
 }
 
 }  // namespace idealgas
